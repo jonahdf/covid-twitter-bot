f2f22f8f2b30fce6dc23b94148f18a4d7d864536
Jonah Fleishhacker <jonah.fleishhacker@gmail.com>
2023-04-27 20:13:35 -0700

    better CLI args

--- src/main.py
+++ src/main.py
@@ -9,16 +9,14 @@ run
  post = 
 runs all scripts to fetch, clean, visualize, and post data
 """
-
-
-def run(post=False, dev=False):
+def run(post=False, to_csv=False, from_csv=False, dev=False):
     # Workaround for Heroku Scheduler: Only run if it's a Sunday
     # Get current time in Pacific Time
     pac_time = datetime.now(timezone(timedelta(hours=-7)))
     if pac_time.weekday() != 6 and not dev:
         print("Not a Sunday. Won't post")
         exit()
-    newestData = data_processing.DataSets()
+    newestData = data_processing.DataSets(from_csv=from_csv, to_csv=to_csv)
     data_viz.generate(newestData)
     if post:
         post_tweets.post()
@@ -27,19 +25,21 @@ def run(post=False, dev=False):
 # Parses command line arguments. Add a -t to post tweets
 if __name__ == "__main__":
     shouldTweet = False
-    shouldDev = False
+    shouldFromCSV = False
+    shouldToCSV = False
+    dev = False
     a = False
     if len(sys.argv) > 1:
-        a = sys.argv[1]
-    if a in ["-t", "--t", "-tweet", "--tweet"]:
+        a = sys.argv[1:]
+        print(a)
+    if "-t" in a:
         shouldTweet = True
-    else:
-        shouldTweet = False
-    if a in ["-d", "--d", "-dev", "--dev"]:
-        shouldDev = True
-    else:
-        shouldDev = False
+    if "-fc" in a:
+        shouldFromCSV = True
+    if "-tc" in a:
+        shouldToCSV = True
+    if "-d" in a:
+        dev = True
 
-    run(post=shouldTweet, dev=shouldDev)
+    run(post=shouldTweet, to_csv=shouldToCSV, from_csv=shouldFromCSV, dev=dev)
 
-run()
